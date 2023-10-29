45862e155669b601aa4ac6d68228bd869ee6bb4d
DmytroVyhovskyi1997 <111079087+DmytroVyhovskyi1997@users.noreply.github.com>
2023-10-29 12:22:57 -0700

    add Loader

--- src/components/TabContent/TabContent.jsx
+++ src/components/TabContent/TabContent.jsx
@@ -1,9 +1,10 @@
 import { Suspense, lazy } from 'react';
+import Loader from '../Loader/Loader'
 
 const TabContent = ({ componentPath, title }) => {
   const TabComponent = lazy(() => import(`../../${componentPath}` /* webpackChunkName: "MovieDetailsPage" */));
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loader/>}>
       <TabComponent array={title.split(' ')} />
     </Suspense>
   );
